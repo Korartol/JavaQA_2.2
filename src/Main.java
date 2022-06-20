82c26f034c41e230bf5944596cf43b4ecb980961
Коробейников Артем <korartol@gmail.com>
2022-06-20 15:11:10 +0300

    Верся 1.0

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,28 @@ public class Main {
         // параметров программы: начального счёта,
         // суммы пополнения и тп
 
+        int balance = 100;
+        int replenishmentAmount = 1000;
+
         // Условным оператором проверяете превысила ли
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+
+        int bonus = replenishmentAmount >= 1000 ? replenishmentAmount / 100 : 0;  // тернарный вариант
+
+/* Длинный вариант решения:
+
+        if (replenishmentAmount >= 1000) {
+            bonus = replenishmentAmount / 100;
+        } else {
+            bonus = 0;
+        }
+*/
+
+        int newBalance = bonus + replenishmentAmount + balance;
+
+        System.out.println("Вам начисленно бонусных рублей - " + bonus);
+        System.out.println("Ваш баланс = " + newBalance);
     }
 }
